153b2a7e0aea45b1383942a13d7df0b7af17692a
wandersonsousa <wandersonsousa3004@gmail.com>
2020-03-14 18:51:36 -0300

    adicionada q9

--- Lista_Fabio_Algoritmo/Fabio02_Parte01/f2_q8_calcula_idade_por_data.py
+++ Lista_Fabio_Algoritmo/Fabio02_Parte01/f2_q8_calcula_idade_por_data.py
@@ -1,5 +1,6 @@
 #Leia data atual (dia, mês e ano) e data de nascimento (dia, mês e ano) de uma pessoa, calcule e escreva sua idade exata (em anos).
 
+
 def main():
     ano_atual = int(input('Digite o ano atual >> '))
     mes_atual = int(input('Digite o mes atual >> '))
@@ -9,9 +10,11 @@ def main():
     mes_nasc = int(input('Digite o mes que nasceu >> '))
     dia_nasc = int(input('Digite o dia que nasceu >> '))
 
-    calcular_idade_atual( ano_atual, mes_atual, dia_atual, ano_nasc, mes_nasc, dia_nasc )
+    calcular_idade_atual(ano_atual, mes_atual, dia_atual,
+                         ano_nasc, mes_nasc, dia_nasc)
 
-def calcular_idade_atual( ano_atual, mes_atual, dia_atual, ano_nasc, mes_nasc, dia_nasc ):
+
+def calcular_idade_atual(ano_atual, mes_atual, dia_atual, ano_nasc, mes_nasc, dia_nasc):
 
     meses_totais_atuais = ano_atual * 12 + mes_atual
     meses_nasc_total = ano_nasc * 12 + mes_nasc
@@ -24,8 +27,7 @@ def calcular_idade_atual( ano_atual, mes_atual, dia_atual, ano_nasc, mes_nasc, d
     else:
         idade_dia = dia_nasc - dia_atual
 
-
     print(f' {idade_ano} anos, {idade_mes} meses e {idade_dia} dias')
-    
+
 
 main()
---
+++ Lista_Fabio_Algoritmo/Fabio02_Parte01/f2_q9_verificar_se_eh_primo.py
@@ -0,0 +1,9 @@
+#Leia 1 (um) número entre 0 e 100, verifique e escreva se o número é ou não primo.
+
+def main():
+    num = int(input('Digite um número >> '))
+
+
+def eh_primo( n ):
+    if()
+main()
