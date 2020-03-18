91fcde5bea44b035fefd16951949dbac5f7617e7
wandersonsousa <wandersonsousa3004@gmail.com>
2020-03-18 14:15:37 -0300

    questao adicionada

--- Lista_Fabio_Algoritmo/Fabio02_Parte01/f2_q9_verificar_se_eh_primo.py
+++ Lista_Fabio_Algoritmo/Fabio02_Parte01/f2_q9_verificar_se_eh_primo.py
@@ -1,9 +1,24 @@
 #Leia 1 (um) número entre 0 e 100, verifique e escreva se o número é ou não primo.
 
 def main():
-    num = int(input('Digite um número >> '))
-
+    if( eh_primo( 0 ) ):
+        print('Primo')
+    else:
+        print('Não é primo')
 
 def eh_primo( n ):
-    if()
+    if( n > 11):
+        if( n % 2 == 0 or n % 3 == 0 or n % 5 == 0 or n % 7 == 0 or n % 11 == 0 ):
+            return False
+        else:
+            return True
+    else:
+        if( n == 2 or n == 3 or n == 5 or n == 7 or n == 11 ):
+            return True
+        else:
+            return False
+
+        
+
+
 main()
