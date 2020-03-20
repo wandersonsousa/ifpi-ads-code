9a4c4114297f31ff7bf9ad9be8201bdba3130331
wandersonsousa <wandersonsousa3004@gmail.com>
2020-03-20 12:13:54 -0300

    questao terminada

--- Lista_Fabio_Algoritmo/Fabio02_Parte01/f2_q19_imc.py
+++ Lista_Fabio_Algoritmo/Fabio02_Parte01/f2_q19_imc.py
@@ -1,4 +1,21 @@
 def main():
+    altura = int( input('Digite sua altura >> ') )
+    peso = int( input('Digite seu peso >> ') )
+
+    imc = ( peso / ( altura ** 2 ) )
+
+    print( analisar_imc(imc) )
+
+
+
+
+def analisar_imc( imc ):
+    if( imc < 25 ):
+        return 'Peso normal'
+    elif imc >= 25 and img < 30:
+        return 'Obeso'
+    elif imc > 30:
+        return 'Obesidade Mórbida'
 
 
 main()
