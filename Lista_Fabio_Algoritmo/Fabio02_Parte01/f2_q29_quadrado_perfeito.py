11bd9c082aacca16f2f62ea82ecf1a34d8cea69f
Wanderson <wandersonsosua3004@gmail.com>
2020-03-21 18:40:13 -0300

    questoes refatoradas

---
+++ Lista_Fabio_Algoritmo/Fabio02_Parte01/f2_q29_quadrado_perfeito.py
@@ -0,0 +1,7 @@
+#Um número é um quadrado perfeito quando a raiz quadrada do número é igual à soma das dezenas formadas pelos seus dois primeiros e dois últimos dígitos.Exemplo: √9801 = 99 = 98 + 01. O número 9801 é um quadrado perfeito. Escreva um algoritmo que leia um número de 4 dígitos e verifique se ele é um quadrado perfeito.
+
+def main():
+    d1 = int(input('Dígitos >> '))
+    d2 = int(input('Dígitos >> '))
+    d3 = int(input('Dígitos >> '))
+    d4 = int(input('Dígitos >> '))
