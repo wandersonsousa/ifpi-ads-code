708e62de974750babe7b290bb13e871b2270a3ce
Wanderson <wandersonsosua3004@gmail.com>
2020-03-22 11:52:38 -0300

    questao terminada

--- Lista_Fabio_Algoritmo/Fabio02_Parte01/f2_q29_quadrado_perfeito.py
+++ Lista_Fabio_Algoritmo/Fabio02_Parte01/f2_q29_quadrado_perfeito.py
@@ -1,7 +1,34 @@
 #Um número é um quadrado perfeito quando a raiz quadrada do número é igual à soma das dezenas formadas pelos seus dois primeiros e dois últimos dígitos.Exemplo: √9801 = 99 = 98 + 01. O número 9801 é um quadrado perfeito. Escreva um algoritmo que leia um número de 4 dígitos e verifique se ele é um quadrado perfeito.
 
+from math import sqrt
+
 def main():
-    d1 = int(input('Dígitos >> '))
-    d2 = int(input('Dígitos >> '))
-    d3 = int(input('Dígitos >> '))
-    d4 = int(input('Dígitos >> '))
+    d1 = str(input('Dígito >> '))
+    d2 = str(input('Dígito >> '))
+    d3 = str(input('Dígito >> '))
+    d4 = str(input('Dígito >> '))
+
+    if eh_quadrado_perfeito( d1, d2, d3, d4 ):
+        print('\nÉ um quadrado perfeito que atende a regra !')
+    else:
+        print('\nNão atende a regra.')
+
+
+def eh_quadrado_perfeito( n1, n2, n3, n4 ):
+
+    numero_completo = digito_para_numero( n1, n2, n3, n4)
+    primeiros_algarismos = digito_para_numero(n1, n2, '', '')
+    ultimos_algarismos = digito_para_numero('', '', n3, n4)
+
+    print( primeiros_algarismos )
+    print(ultimos_algarismos)
+    print(numero_completo)
+
+    return sqrt( numero_completo ) == (primeiros_algarismos + ultimos_algarismos)
+
+def digito_para_numero( n1 , n2 , n3, n4):
+    num_str = n1 + n2 + n3 + n4
+    return int(num_str)
+
+main()
+ 
